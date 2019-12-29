ced90837409194a37e8f214ba4c2c16f32c0cd8c
Fabian Foerg <3429782+faf0@users.noreply.github.com>
2019-12-29 10:57:02 -0500

    use Bash functions

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 
 set -euxo pipefail
 
-[[ "$(uname)" = 'Darwin' ]] || \
-  ( echo 'Must be run on macOS' ; exit 1 )
-
 readonly IMAGE_FILE='2019-09-26-raspbian-buster-lite.zip'
 readonly IMAGE_URL="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-09-30/${IMAGE_FILE}"
 readonly KERNEL_URL='https://github.com/dhruvvyas90/qemu-rpi-kernel/blob/c5a491c093604a71db2f01b8fab72bad0e96e2b5/kernel-qemu-4.19.50-buster?raw=true'
@@ -14,16 +11,48 @@ readonly TMP_DIR="${HOME}/qemu_vms"
 readonly RPI_KERNEL="${TMP_DIR}/kernel-qemu-4.19.50-buster"
 readonly PTB_FILE="${TMP_DIR}/versatile-pb.dtb"
 
-command -v brew &> /dev/null || \
-  ( echo 'Install homebrew' ; exit 1 )
+check_commands () {
+  [[ "$(uname)" = 'Darwin' ]] || \
+    ( echo 'Must be run on macOS' ; exit 1 )
+  
+  command -v brew &> /dev/null || \
+    ( echo 'Install homebrew' ; exit 1 )
+  
+  command -v curl &> /dev/null || \
+    ( echo 'Install curl' ; exit 1 )
+  
+  command -v unzip &> /dev/null || \
+    ( echo 'Install unzip' ; exit 1 )
+}
 
-( mkdir -p "$TMP_DIR" && \
-  cd "$TMP_DIR" ) || \
-  exit 1
+install_qemu () {
+  command -v qemu-system-arm &> /dev/null || \
+    brew install qemu
+}
 
-[[ -f "$RPI_KERNEL" ]] || curl -sSL "$KERNEL_URL" -o "$RPI_KERNEL"
-[[ -f "$PTB_FILE" ]] || curl -sSL "$PTB_URL" -o "$PTB_FILE"
-[[ -f "$IMAGE_FILE" ]] || curl -sSL "$IMAGE_URL" -o "$IMAGE_FILE"
-[[ -f "${IMAGE_FILE%.*}.img" ]] || unzip "$IMAGE_FILE"
+change_dir () {
+  ( mkdir -p "$TMP_DIR" && \
+    cd "$TMP_DIR" ) || \
+    exit 1
+}
 
-command -v qemu-system-arm &> /dev/null || brew install qemu
+exctract_images () {
+  [[ -f "$RPI_KERNEL" ]] || \
+    curl -sSL "$KERNEL_URL" -o "$RPI_KERNEL"
+  [[ -f "$PTB_FILE" ]] || \
+    curl -sSL "$PTB_URL" -o "$PTB_FILE"
+  [[ -f "$IMAGE_FILE" ]] || \
+    curl -sSL "$IMAGE_URL" -o "$IMAGE_FILE"
+  [[ -f "${IMAGE_FILE%.*}.img" ]] || \
+    unzip "$IMAGE_FILE"
+}
+
+main () {
+  check_commands
+  install_qemu
+  change_dir
+  exctract_images
+}
+
+main
+
--- run.sh
+++ run.sh
@@ -2,22 +2,29 @@
 
 set -euxo pipefail
 
-[[ "$(uname)" = 'Darwin' ]] || \
-  ( echo 'Must be run on macOS' ; exit 1 )
-
-readonly QEMU=$(which qemu-system-arm)
-
-[[ -z "$QEMU" ]] && \
-  ( echo 'Install qemu' ; exit 1 )
-
+readonly QEMU_SYS='qemu-system-arm'
 readonly TMP_DIR="${HOME}/qemu_vms"
 readonly RPI_KERNEL="${TMP_DIR}/kernel-qemu-4.19.50-buster"
 readonly RPI_FS="${TMP_DIR}/2019-09-26-raspbian-buster-lite.img"
 readonly PTB_FILE="${TMP_DIR}/versatile-pb.dtb"
 
-"$QEMU" -kernel "$RPI_KERNEL" \
-  -cpu arm1176 -m 256 -M versatilepb \
-  -dtb "$PTB_FILE" -no-reboot \
-  -serial stdio -append 'root=/dev/sda2 panic=1 rootfstype=ext4 rw' \
-  -drive "file=${RPI_FS},index=0,media=disk,format=raw" \
-  -net user,hostfwd=tcp::5022-:22 -net nic
+has_qemu () {
+  command -v "$QEMU_SYS" &> /dev/null || \
+    ( echo 'Install QEMU' ; exit 1 )
+}
+
+run_qemu () {
+  "$QEMU_SYS" -kernel "$RPI_KERNEL" \
+    -cpu arm1176 -m 256 -M versatilepb \
+    -dtb "$PTB_FILE" -no-reboot \
+    -serial stdio -append 'root=/dev/sda2 panic=1 rootfstype=ext4 rw' \
+    -drive "file=${RPI_FS},index=0,media=disk,format=raw" \
+    -net user,hostfwd=tcp::5022-:22 -net nic
+}
+
+main () {
+  has_qemu
+  run_qemu
+}
+
+main
