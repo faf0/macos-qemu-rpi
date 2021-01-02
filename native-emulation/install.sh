472091b241b6f2cfeae88b70f23927b23d76fad0
Fabian Foerg <3429782+faf0@users.noreply.github.com>
2021-01-01 20:36:20 -0500

    Add support for qemu raspi3 native emulation

--- native-emulation/install.sh
+++ native-emulation/install.sh
@@ -3,10 +3,10 @@
 set -euxo pipefail
 
 readonly IMAGE='2020-02-13-raspbian-buster-lite'
-readonly KERNEL='kernel-qemu-5.4.51-buster'
-readonly PTB='versatile-pb-buster-5.4.51.dtb'
+readonly KERNEL='kernel8.img'
+readonly PTB='bcm2710-rpi-3-b-plus.dtb'
 
-readonly TMP_DIR="${HOME}/qemu_vms"
+readonly TMP_DIR="${HOME}/qemu_vms_native"
 readonly KERNEL_FILE="${TMP_DIR}/${KERNEL}"
 readonly PTB_FILE="${TMP_DIR}/${PTB}"
 
@@ -14,8 +14,8 @@ readonly PTB_FILE="${TMP_DIR}/${PTB}"
 readonly COMMIT_HASH='061a3853cf2e2390046d163d90181bde1c4cd78f'
 
 readonly IMAGE_URL="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2020-02-14/${IMAGE}.zip"
-readonly KERNEL_URL="https://github.com/dhruvvyas90/qemu-rpi-kernel/blob/${COMMIT_HASH}/${KERNEL}?raw=true"
-readonly PTB_URL="https://github.com/dhruvvyas90/qemu-rpi-kernel/blob/${COMMIT_HASH}/${PTB}?raw=true"
+readonly KERNEL_URL="https://github.com/dhruvvyas90/qemu-rpi-kernel/blob/${COMMIT_HASH}/native-emuation/5.4.51%20kernels/${KERNEL}?raw=true"
+readonly PTB_URL="https://github.com/dhruvvyas90/qemu-rpi-kernel/blob/${COMMIT_HASH}/native-emuation/dtbs/${PTB}?raw=true"
 
 check_commands () {
   [ "$(uname)" = 'Darwin' ] || \
@@ -32,7 +32,7 @@ check_commands () {
 }
 
 install_qemu () {
-  command -v qemu-system-arm &> /dev/null || \
+  command -v qemu-system-aarch64 &> /dev/null || \
     brew install qemu
 }
 
@@ -51,6 +51,8 @@ exctract_images () {
     curl -sSL "$IMAGE_URL" -o "${IMAGE}.zip"
   [ -f "${IMAGE}.img" ] || \
     unzip "${IMAGE}.zip"
+  [ -f "${IMAGE}.img" ] || \
+    qemu-img resize -f raw "${IMAGE}.img" 2G
 }
 
 main () {
--- native-emulation/run.sh
+++ native-emulation/run.sh
@@ -3,15 +3,15 @@
 set -euxo pipefail
 
 readonly IMAGE='2020-02-13-raspbian-buster-lite'
-readonly KERNEL='kernel-qemu-5.4.51-buster'
-readonly PTB='versatile-pb-buster-5.4.51.dtb'
+readonly KERNEL='kernel8.img'
+readonly PTB='bcm2710-rpi-3-b-plus.dtb'
 
-readonly TMP_DIR="${HOME}/qemu_vms"
+readonly TMP_DIR="${HOME}/qemu_vms_native"
 readonly IMAGE_FILE="${TMP_DIR}/${IMAGE}.img"
 readonly KERNEL_FILE="${TMP_DIR}/${KERNEL}"
 readonly PTB_FILE="${TMP_DIR}/${PTB}"
 
-readonly QEMU_SYS='qemu-system-arm'
+readonly QEMU_SYS='qemu-system-aarch64'
 
 has_qemu () {
   command -v "$QEMU_SYS" &> /dev/null || \
@@ -20,19 +20,21 @@ has_qemu () {
 
 run_qemu () {
   "$QEMU_SYS" \
-    -cpu arm1176 \
-    -m 256 \
-    -M versatilepb \
+    -m 1G \
+    -M raspi3 \
+    -smp 4 \
     -serial stdio \
-    -drive "file=${IMAGE_FILE},if=none,index=0,media=disk,format=raw,id=disk0" \
-    -device 'virtio-blk-pci,drive=disk0,disable-modern=on,disable-legacy=off' \
+    -usb \
+    -device usb-mouse \
+    -device usb-kbd \
+    -sd "$IMAGE_FILE" \
     -net 'user,hostfwd=tcp::5022-:22' \
     -net nic \
     -dtb "$PTB_FILE" \
     -kernel "$KERNEL_FILE" \
-    -append 'root=/dev/vda2 panic=1' \
+    -append 'rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1' \
     -no-reboot
-}
+} 
 
 main () {
   has_qemu
